4d1fe66a587940ecf8ddb392ab2c3935370952f1
deicibuitrago503-design <deicibuitrago503@gmail.com>
2025-11-18 14:21:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    Fijar_1 += 1
+    basic.showNumber(Fijar_1)
+})
+let Fijar_1 = 0
+basic.showNumber(0)
+Fijar_1 = 0
+basic.forever(function () {
+	
+})
